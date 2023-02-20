9ab433f902e622786d22d264b40a0333f107fa6c
ThisIsKhalid <iamkhalid.hs@gmail.com>
2023-02-20 12:02:51 +0600

    update nab

--- src/pages/Shared/Navbar.js
+++ src/pages/Shared/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { FiMenu, FiShoppingCart } from "react-icons/fi";
+import { FiMenu, FiSearch, FiShoppingCart } from "react-icons/fi";
 import { Link } from "react-router-dom";
 import logo from "../../assets/images/electronic-cigarette.png";
 // drawer
@@ -7,8 +7,8 @@ import Drawer from "react-modern-drawer";
 import "react-modern-drawer/dist/index.css";
 
 const Navbar = () => {
-
   const [isOpen, setIsOpen] = useState(false);
+  const [searchOpen, setSearchOpen] = useState(false);
   const toggleDrawer = () => {
     setIsOpen((prevState) => !prevState);
   };
@@ -31,7 +31,29 @@ const Navbar = () => {
         <div className="text-gray-200 text-lg md:flex items-center gap-3 hidden">
           <Link>Register</Link>
           <Link>Login</Link>
-          <div className="flex items-center gap-1">
+
+          <div className="cursor-pointer relative">
+            <FiSearch
+              onClick={() => setSearchOpen(!searchOpen)}
+              className="text-2xl"
+            />
+            {searchOpen && (
+              <input
+                onKeyDown={(event) => {
+                  if (event.key === "Enter") {
+                    setSearchOpen(!searchOpen);
+                  }
+                }}
+                className="bg-gray-200 py-2 text-gray-900 px-5 rounded focus:outline-none text-base absolute -bottom-12 -left-64 z-50 transition ease-in-out delay-150 duration-300"
+                type="text"
+                name=""
+                id=""
+                placeholder="search your item, product"
+              />
+            )}
+          </div>
+
+          <div className="flex items-center gap-1 cursor-pointer">
             <FiShoppingCart className="text-2xl" />
             <p className="bg-gray-200 px-1 font-medium rounded-full text-LightCoral">
               0
@@ -45,14 +67,14 @@ const Navbar = () => {
         >
           <FiMenu className="text-2xl text-gray-200" />
           <Drawer
-                open={isOpen}
-                onClose={toggleDrawer}
-                direction='right'
-                size='50%'
-                className='bla bla bla'
-            >
-                <div className="h-full w-full bg-MidnightBlue/80">Hello World</div>
-            </Drawer>
+            open={isOpen}
+            onClose={toggleDrawer}
+            direction="right"
+            size="50%"
+            className="bla bla bla"
+          >
+            <div className="h-full w-full bg-MidnightBlue/80">Hello World</div>
+          </Drawer>
         </div>
       </section>
     </>
